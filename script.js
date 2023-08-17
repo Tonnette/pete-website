6795c570f75f9683f6cc930996611b9e51155a54
Tonnette <tonnettestanford@yahoo.com.au>
2023-08-17 19:30:24 +1000

    added text animation

--- script.js
+++ script.js
@@ -4,26 +4,34 @@ const right = document.querySelector('.menubtn')
 const container = document.querySelector('.container')
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
-var menu = document.querySelector('.menu-items')
+var menu = document.querySelector('.menu-items');
+var textFade =  document.querySelector('.name');
+var textFade2 =  document.querySelector('.name2');
+var animateText = document.querySelector('.animationClass');
+
+window.onload = function() {
+  window.setTimeout(fadeup, 1000); //1 seconds
+}
+
+function fadeup() {
+  textFade.style.opacity = '1';
+  textFade2.style.opacity = '1';
+  animateText.style.opacity = '1';
+
+}
+
+
 
 
 
 function menuFunction() {
   container.classList.add('hover-left')
   container.classList.remove('hover-right')
-
-
-
-  
 }
 
 function hideFunction() {
   container.classList.add('hover-right')
   container.classList.remove('hover-left')
-
-
-  
- 
 }
 
         
